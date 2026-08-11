a6117181ed2afe7b09234aed5d4802e7068fa2d1
Sidhartha Kar <sidharthashankarkar@gmail.com>
2026-08-11 05:50:13 +0530

    add cucumber

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,7 +2,9 @@ const{test,expect} = require('@playwright/test')
 const {ActionsUtility} = require('../utility/ActionsUtility')
 const {LoginPage} = require('../pages/LoginPage')
 const data_login = require('../data.json')
-const { request } = require('node:http')
+const { request, STATUS_CODES } = require('node:http')
+const { json } = require('node:stream/consumers')
+const { fail } = require('node:assert')
 
 for(const user_data of data_login.login){
 test(`Open Browser ${user_data.username}`,async({page})=>{    
@@ -18,4 +20,28 @@ test(`Open Browser ${user_data.username}`,async({page})=>{
 test('Open Crm Applocation',async({page})=>{
     await page.goto('/');
     console.log(await page.title())
+    const loginpage = new LoginPage(page);
+    loginpage.logintoSausdemo('Admin','admin123')
+
+    page.route('',
+        async route=>{
+            const respone = await page.request.fetch(route.request())
+            route.fulfil({
+                respone,
+                body:JSON.stringify(""),
+                staus:200
+        })
+           
+    })
+
+    page.route('',async route=>{
+        route.continue({headers:
+            {"":""},
+            url:""})
+    })
+
+    page.route('',async route=>{
+        route.abort('failed')
+    } )
+
 })
